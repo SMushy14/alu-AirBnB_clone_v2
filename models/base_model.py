5ad9852292a6699eb657b7fb7b32de73a78af04c
Sharon Mushy <155962275+SMushy14@users.noreply.github.com>
2024-09-27 12:38:07 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,17 +18,20 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
+        expected_keys = ['id', 'created_at', 'updated_at']  # Add other expected keys if necessary
+
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
+                if key not in expected_keys and key != '__class__':
+                    raise KeyError(f"Unexpected key: {key}")
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
             # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
             if not hasattr(kwargs, 'id'):
                 setattr(self, 'id', str(uuid.uuid4()))
