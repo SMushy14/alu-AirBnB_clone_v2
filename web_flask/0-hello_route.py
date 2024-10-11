249ae5cb0e85f2d778f1081fa8461cd0b4f0d5b9
root <s.mushy1@alustudent.com>
2024-10-11 11:50:50 +0000

    readme file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""a script that starts a Flask web application listening on 0.0.0.0, port 5000 display Hello HBNB! and uses the option strict_slashes=False in your route definition """
 from flask import Flask
 
 my_app = Flask(__name__)
