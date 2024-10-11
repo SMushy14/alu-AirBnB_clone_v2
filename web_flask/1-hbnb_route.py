67624e342e70e9271a836c3018f438c6d2a9a101
root <s.mushy1@alustudent.com>
2024-10-11 12:11:55 +0000

    task1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""A script that starts a Flask web application
+listening on 0.0.0.0, port 5000
+and also at the /hbnb route. display Hello HBNB! """
+
+from flask import Flask
+
+my_app = Flask(__name__)
+
+
+@my_app.route('/', strict_slashes=False)
+@my_app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    my_app.run(host="0.0.0.0", port=5000)
