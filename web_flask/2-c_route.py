c6cabaf4753fb0f1925493b27924049050e6f177
root <s.mushy1@alustudent.com>
2024-10-11 12:42:08 +0000

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 """A script that starts a Flask web application
-listening on 0.0.0.0, port 5000
-and also at the /hbnb route. display Hello HBNB! """
+listening on 0.0.0.0, port 5000 at the /route display
+Hello HBNB!, at /hbnb route display HBNB and at
+/c/<text> display C followed by the value of the
+text variable, replace underscore _ symbols with a space"""
 
 from flask import Flask
 
@@ -17,6 +19,11 @@ def hello_hbnb():
 @my_app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
+
+@my_app.route('/c/<text>', strict_slashes=False)
+def text_display():
+    return "C {}".format(text.replace('_', ' '))
 
 if __name__ == "__main__":
     my_app.run(host="0.0.0.0", port=5000)
