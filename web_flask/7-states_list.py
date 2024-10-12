3cc1884ca13b16fab0d221d52137dc69e9a9ed29
Sharon Mushy <155962275+SMushy14@users.noreply.github.com>
2024-10-12 17:00:56 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""Module that starts a Flask web application.
-This module sets up a simple Flask web application that provides a route
-to display a list of states stored in a database. The states are fetched
-from the storage, sorted alphabetically, and rendered in an HTML template.
-"""
+"""A script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+LI tag: description of one State: <state.id>: <B><state.name></B>
+Import this 7-dump to have some data
+You must use the option strict_slashes=False in your route definition"""
 
 from flask import Flask, render_template
 from models import storage
@@ -14,26 +22,12 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Returns an HTML page with a list of all States in the database.
-    This route handles GET requests to '/states_list'. It retrieves all
-    states from the database, sorts them by name, and renders them using
-    the '7-states_list.html' template.
-    Returns:
-        str: Rendered HTML template containing the list of states.
-    """
     states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def close_session(exception):
-    """Closes the session after each request.
-    This function is called after each request, ensuring that any
-    resources allocated during the request (e.g., database sessions)
-    are properly closed.
-    Args:
-        exception: The exception raised during the request, if any.
-    """
     storage.close()
 
 
