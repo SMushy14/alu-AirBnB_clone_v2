6324aa933e4ce1a3c8545cc430a7397a468af9b7
Sharon Mushy <155962275+SMushy14@users.noreply.github.com>
2024-10-12 16:44:36 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Returns an HTML page with a list of all States in the database"""
+"""Returns an HTML page with a list of all States in the database"""
     states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def close_session(exception):
-    """Closes the session"""
+"""Closes the session"""
     storage.close()
 
 
